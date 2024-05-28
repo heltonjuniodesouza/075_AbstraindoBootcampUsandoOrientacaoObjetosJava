9aa31605b0c54a8e745476097cf1b2ff76eaa412
heltonjuniodesouza <heltonjuniosouza@gmail.com>
2024-05-28 19:54:39 -0300

    ABSTRAINDO ORIENTAÇÕES JAVA DIO

--- TrabalhoEmCurso/desafio-poo-dio/src/br/com/dio/desafio/dominio/Conteudo.java
+++ TrabalhoEmCurso/desafio-poo-dio/src/br/com/dio/desafio/dominio/Conteudo.java
@@ -1,27 +1,27 @@
-package br.com.dio.desafio.dominio;
+    package br.com.dio.desafio.dominio;
 
-public abstract class Conteudo {
+    public abstract class Conteudo {
 
-    protected static final double XP_PADRAO = 10d;
+        protected static final double XP_PADRAO = 10d;
 
-    private String titulo;
-    private String descricao;
+        private String titulo;
+        private String descricao;
 
-    public abstract double calcularXp();
+        public abstract double calcularXp();
 
-    public String getTitulo() {
-        return titulo;
+        public String getTitulo() {
+            return titulo;
+        }
+
+        public void setTitulo(String titulo) {
+            this.titulo = titulo;
+        }
+
+        public String getDescricao() {
+            return descricao;
+        }
+
+        public void setDescricao(String descricao) {
+            this.descricao = descricao;
+        }
     }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-}
